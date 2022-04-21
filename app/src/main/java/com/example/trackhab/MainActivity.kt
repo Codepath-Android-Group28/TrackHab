40b54a75a794ab456e0add9afa99f379593675bb
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-04-21 10:03:53 -0700

    parse server working now

--- app/src/main/java/com/example/trackhab/MainActivity.kt
+++ app/src/main/java/com/example/trackhab/MainActivity.kt
@@ -15,22 +15,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val person = ParseObject("Person")
-        person.put("name", "John Snow")
-        person.put("age", 27)
-        person.saveInBackground()
-
-        val query = ParseQuery.getQuery<ParseObject>("Person")
-        query.getInBackground("mhPFDlCahj") { `object`, e ->
-            if (e == null) {
-                // object will be your person
-                Log.i("MainActivity1", "SUCCESS")
-            } else {
-                // something went wrong
-                Log.i("MainActivity1", "FAIL: " + e)
-            }
-        }
-    }
+        
 }
