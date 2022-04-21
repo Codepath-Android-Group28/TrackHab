09bca99c9ae002ff49d1af76df7acbd2d3e61365
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-04-20 23:16:19 -0700

    sets up parse server (doesn't work on emulator)

--- app/src/main/java/com/example/trackhab/MainActivity.kt
+++ app/src/main/java/com/example/trackhab/MainActivity.kt
@@ -2,12 +2,35 @@ package com.example.trackhab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.parse.ParseObject
+import com.parse.GetCallback
+
+import com.parse.ParseQuery
+
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Testing Commits
+
+        val person = ParseObject("Person")
+        person.put("name", "John Snow")
+        person.put("age", 27)
+        person.saveInBackground()
+
+        val query = ParseQuery.getQuery<ParseObject>("Person")
+        query.getInBackground("mhPFDlCahj") { `object`, e ->
+            if (e == null) {
+                // object will be your person
+                Log.i("MainActivity1", "SUCCESS")
+            } else {
+                // something went wrong
+                Log.i("MainActivity1", "FAIL: " + e)
+            }
+        }
     }
 }
