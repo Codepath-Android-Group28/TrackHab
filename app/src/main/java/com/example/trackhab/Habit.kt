001d25ebcb736aa356b70f91210e60b54572462f
Gevork Manukyan <gevork.manukyan.297@my.csun.edu>
2022-04-21 12:43:45 -0700

    adds weekstatus to habit object

--- app/src/main/java/com/example/trackhab/Habit.kt
+++ app/src/main/java/com/example/trackhab/Habit.kt
@@ -72,6 +72,28 @@ class Habit : ParseObject() {
         put(KEY_COMPLETED, completed)
     }
 
+    fun getWeeklyStatus(): BooleanArray {
+        val monday = getBoolean(KEY_MONDAY)
+        val tuesday = getBoolean(KEY_TUESDAY)
+        val wednesday = getBoolean(KEY_WEDNESDAY)
+        val thursday = getBoolean(KEY_THURSDAY)
+        val friday = getBoolean(KEY_FRIDAY)
+        val saturday = getBoolean(KEY_SATURDAY)
+        val sunday = getBoolean(KEY_SUNDAY)
+        val statusArray: BooleanArray = booleanArrayOf( monday, tuesday, wednesday, thursday, friday, saturday, sunday )
+        return statusArray;
+    }
+
+    fun setWeeklyStatus(statusArray: BooleanArray) {
+        put(KEY_MONDAY, statusArray[0])
+        put(KEY_TUESDAY, statusArray[1])
+        put(KEY_WEDNESDAY, statusArray[2])
+        put(KEY_THURSDAY, statusArray[3])
+        put(KEY_FRIDAY, statusArray[4])
+        put(KEY_SATURDAY, statusArray[5])
+        put(KEY_SUNDAY, statusArray[6])
+    }
+
     companion object {
         const val KEY_USER = "user"
         const val KEY_HABIT_NAME = "habitName"
@@ -81,5 +103,12 @@ class Habit : ParseObject() {
         const val KEY_TARGET = "target"
         const val KEY_UNIT = "unit"
         const val KEY_COMPLETED = "completed"
+        const val KEY_MONDAY = "monday"
+        const val KEY_TUESDAY = "tuesday"
+        const val KEY_WEDNESDAY = "wednesday"
+        const val KEY_THURSDAY = "thursday"
+        const val KEY_FRIDAY = "friday"
+        const val KEY_SATURDAY = "saturday"
+        const val KEY_SUNDAY = "sunday"
     }
 }
--- app/src/main/java/com/example/trackhab/fragments/StatsFragment.kt
+++ app/src/main/java/com/example/trackhab/fragments/StatsFragment.kt
@@ -1,11 +1,16 @@
 package com.example.trackhab.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.trackhab.Habit
 import com.example.trackhab.R
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
 
 class StatsFragment : Fragment() {
 
@@ -18,6 +23,29 @@ class StatsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        queryUserHabits()
+    }
+
+    private fun queryUserHabits() {
+
+        val query: ParseQuery<Habit> = ParseQuery.getQuery(Habit::class.java)
+
+        // Might be able to get this data from a different query in a different fragment
+        query.include(Habit.KEY_USER)
+
+        query.findInBackground(object: FindCallback<Habit> {
+            override fun done(objects: MutableList<Habit>?, e: ParseException?) {
+                if (e == null) {
+                    // TODO: Successful query
+
+                } else {
+                    Log.e("StatsFragment", "Error fetching posts")
+                    e.printStackTrace()
+                }
+            }
+
+        })
     }
 }
